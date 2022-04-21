09169d2c2f7704d2ea32240b74d6c8ce2d94996f
prasanna1234567890 <prasannaspring2022@gmail.com>
2022-04-20 21:36:08 -0500

    needed

--- app.js
+++ app.js
@@ -4,11 +4,47 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bakeryRouter = require('./routes/bakery');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var bakery = require("./models/bakery");
+var resourceRouter = require('./routes/resource');
+
+// We can seed the collection if needed on server start
+async function recreateDB(){
+  // Delete everything
+  await bakery.deleteMany();
+  let instance1 = new bakery({Itemname:"sandwich", Quantity:50,
+  price:"Ten USD"});
+  instance1.save( function(err,doc) {
+    if(err) return console.error(err);
+    console.log("First object saved")
+  });
+  let instance2 = new bakery({Itemname:"donut", Quantity:20,
+  price:"Five USD"});
+  instance2.save( function(err,doc) {
+    if(err) return console.error(err);
+    console.log("Second object saved")
+  });
+  let instance3 = new bakery({Itemname:"Coke", Quantity:1,
+  price:"Ten USD"});
+  instance3.save( function(err,doc) {
+    if(err) return console.error(err);
+    console.log("Third object saved")
+  });
+  }
+  let reseed = true;
+  if (reseed) { recreateDB();}
+
 
 var app = express();
 
@@ -27,6 +63,7 @@ app.use('/users', usersRouter);
 app.use('/bakery', bakeryRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource',resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,5 +80,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+console.log("Connection to DB succeeded")}); 
 
 module.exports = app;
--- routes/bakery.js
+++ routes/bakery.js
@@ -1,9 +1,29 @@
 var express = require('express');
+const bakery_controlers= require('../controllers/bakery');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('bakery', { title: 'Search Results bakery' });
-});
+// A little function to check if we have an authorized user and continue on
+//or redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
+/* GET bakerys */
+router.get('/', bakery_controlers.bakery_view_all_Page );
 module.exports = router;
+
+/* GET detail bakery page */
+router.get('/detail', bakery_controlers.bakery_view_one_Page);
+
+/* GET create bakery page */
+router.get('/create',secured, bakery_controlers.bakery_create_Page);
+
+/* GET create update page */
+router.get('/update',secured, bakery_controlers.bakery_update_Page);
+
+/* GET create bakery page */
+router.get('/delete',secured, bakery_controlers.bakery_delete_Page);
