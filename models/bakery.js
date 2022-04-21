f9ebe3845c3f8cf0fc35dda46228007b41266805
prasanna1234567890 <prasannaspring2022@gmail.com>
2022-04-20 21:26:13 -0500

    change needed

---
+++ models/bakery.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const bakerySchema = mongoose.Schema({
+Itemname: {
+        type: String,
+        minlength: 4
+},
+Quantity: Number,
+price: {
+        type: String,
+        minLength: 5
+}
+})
+module.exports = mongoose.model("bakery", bakerySchema)
